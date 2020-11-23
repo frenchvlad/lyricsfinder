2ea24ce4183915d0a1153e355e6591ce02bb9712
frenchvlad <71971673+frenchvlad@users.noreply.github.com>
2020-11-23 18:54:00 +0100

    update index.js // simple javascript to jQuery

--- assets/index.js
+++ assets/index.js
@@ -1,34 +1,22 @@
-const form = document.querySelector("form");
-const author = document.getElementById("author");
-const music = document.getElementById("music");
-const divLyrics = document.getElementById("lyrics");
-const paragrapheLyrics = document.createElement("p");
-paragrapheLyrics.setAttribute("id","plyrics");
-
-function escapeHtml(text) {
-    return text
-        .replace(/&/g, "&amp;")
-        .replace(/</g, "&lt;")
-        .replace(/>/g, "&gt;");
-  }
-  
-form.addEventListener("submit", (e) => {
-    e.preventDefault();
-    author.value = escapeHtml(author.value);
-    music.value = escapeHtml(music.value);
-    axios.get(`https://api.lyrics.ovh/v1/${author.value}/${music.value}`)
-    .then((response) => {
-        if(response.data.lyrics !== "") {
-            console.log(response.data.lyrics);
-            paragrapheLyrics.innerHTML = `<b>Voici les paroles de ${author.value} - ${music.value}</b><br><br>${response.data.lyrics}`;
-            divLyrics.append(paragrapheLyrics);
-        }
-        else {
-            paragrapheLyrics.innerHTML = "La chanson n'est pas présente dans la base de donnée :(";
-            divLyrics.append(paragrapheLyrics);
-        }
-    })
-    .catch((error) => {
-        // :)
-    })
-})
+$(() => {
+    $("form").submit((e) => {
+        e.preventDefault();
+        function escapeHtml(text) {
+            return text
+                .replace(/&/g, "&amp;")
+                .replace(/</g, "&lt;")
+                .replace(/>/g, "&gt;");
+          }
+        let author = escapeHtml($("#author").val());
+        let music = escapeHtml($("#music").val());
+        $.get(`https://api.lyrics.ovh/v1/${author}/${music}`, (data) => {
+            let lyrics = data.lyrics;
+            if(lyrics !== "") {
+                $("#lyrics").html(`<p id='pres'><b>Voici les paroles de ${author} - ${music}</b></p><p id='plyrics'>${lyrics}</p>`);
+            }
+            else {
+                $("#lyrics").html("<p id='plyrics'>La chanson n'est pas présente dans la base de donnée :(</p>");
+            }
+        });
+    });
+});
