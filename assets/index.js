d271740eeb320c799e030e1958f610f6fdafa5f0
frenchvlad <71971673+frenchvlad@users.noreply.github.com>
2020-11-23 01:23:24 +0100

    Update index.js

--- assets/index.js
+++ assets/index.js
@@ -9,9 +9,7 @@ function escapeHtml(text) {
     return text
         .replace(/&/g, "&amp;")
         .replace(/</g, "&lt;")
-        .replace(/>/g, "&gt;")
-        .replace(/"/g, "&quot;")
-        .replace(/'/g, "&#039;");
+        .replace(/>/g, "&gt;");
   }
   
 form.addEventListener("submit", (e) => {
